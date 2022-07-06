cbdc4e4b850a4f6aa6bc873e5b1d736e783446e5
arpgs <zqg918@hotmail.com>
2022-07-06 11:00:58 +0800

    modfiy config app

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# 修改默认IP为192.168.123.1
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate 
